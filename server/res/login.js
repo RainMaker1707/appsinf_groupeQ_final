3fe5beee6702c5d0629b1fad91adcf589d2a6626
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2020-11-09 23:18:23 +0100

    pattern debug for login/signup

--- server/res/login.js
+++ server/res/login.js
@@ -3,12 +3,19 @@ let bcrypt = require('bcrypt');
 module.exports = function login(req, res, db){
     db.db('amagus').collection('users').findOne({pseudo: req.body.pseudoIn}, (err, doc)=>{
         if(err) throw err;
-        else if(doc === null) res.redirect('/'); // pseudo not registered
-        else{
+        else if(doc === null) {
+            console.log('pseudo not registered'); // TODO DEBUG
+            res.redirect('/');
+        }else{
             bcrypt.compare(req.body.passIn, doc.password, (err, check)=>{
                 if(err) throw err;
-                else if (!check) res.redirect('/'); //Bad password
-                else res.redirect('/userPage'); // All ok connection ==>
+                else if (!check) {
+                    console.log('bad password'); // TODO DEBUG
+                    res.redirect('/');
+                }else {
+                    console.log('connected'); //TODO DEBUG
+                    res.redirect('/userPage');
+                }
             })
         }
     })
--- server/res/signup.js
+++ server/res/signup.js
@@ -1,5 +1,14 @@
 let bcrypt = require('bcrypt');
 
 module.exports = function sign(req, res, db){
-    res.redirect('/userPage');
+    db.db('amagus').collection('users').findOne({email: req.body.mailIn}, (err, doc)=>{
+        if(err) throw err;
+        if(doc !== null){
+            console.log('mail already registered');// TODO debug
+            res.redirect('/');
+        }else{
+            console.log('ok now signed in'); //TODO debug
+            res.redirect('/editUser');
+        }
+    });
 };
