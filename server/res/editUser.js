0d246b9069c5546e3d601cfae0b3759fafccfeea
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2020-12-18 19:46:59 +0100

    remove change ssh key

--- server/res/editUser.js
+++ server/res/editUser.js
@@ -1,7 +1,5 @@
 let bcrypt = require('bcrypt');
 let login = require('./login');
-let Mail = require('./mail');
-let {generateKeyPair} = require('crypto');
 
 module.exports = function editUser(req, res, db){
     db.db('amagus').collection('users').findOne({pseudo: req.session.pseudo}, (err, doc) => {
@@ -29,40 +27,12 @@ module.exports = function editUser(req, res, db){
                     bcrypt.genSalt(10, (err, salt) => {
                         if (err) throw err;
                         bcrypt.hash(req.body.password, salt, (err, hash) => {
-                            bcrypt.hash(req.session.username + Math.random() + req.session.mail + Math.random(), salt, (err, key) => {
-                                if (err) throw err;
-                                key = key.replace(/\$/g, '');
-
-                                generateKeyPair('rsa', {
-                                        modulusLength: 8192,
-                                        publicKeyEncoding: {
-                                            type: 'spki',
-                                            format: 'pem'
-                                        },
-                                        privateKeyEncoding: {
-                                            type: 'pkcs8',
-                                            format: 'pem',
-                                            cipher: 'aes-256-cbc',
-                                            passphrase: key
-                                        }
-                                    },
-                                    (err, publicKey, privateKey) => {
-                                        if (err) throw err;
-                                        else {
-                                            db.db('amagus').collection('users').updateOne({pseudo: req.session.pseudo},
-                                                {$set: {password: hash, uniqKey: key, publicKey: privateKey, privateKey: privateKey, favoriteMap: req.body.maps, favoriteColor: req.body.colors, country: req.body.country, language: req.body.language}});
-                                            let mail = new Mail();
-                                            // doc.activated = false;
-                                            mail.send(req.session.mail,
-                                                "Password Changed",
-                                                "Your password has been changed, please click on the link to confirm it: \n" +
-                                                "https://localhost/confirm?key=" + key + "&user=" + req.session.pseudo
-                                            );
-                                            // login(req, res, db, true);
-                                            res.redirect('/user-page?user=' + req.session.pseudo);
-                                        }
-                                    });
-                            });
+                            if (err) throw err;
+                                db.db('amagus').collection('users').updateOne({pseudo: req.session.pseudo},
+                                    {$set: {password: hash, favoriteMap: req.body.maps, favoriteColor: req.body.colors, country: req.body.country, language: req.body.language}});
+                                // doc.activated = false;
+                                // login(req, res, db, true);
+                                res.redirect('/user-page?user=' + req.session.pseudo);
                         });
                     });
                 }
